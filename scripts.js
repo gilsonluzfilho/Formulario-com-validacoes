55dee417a2118adef61f822f63551774e5d120a1
gilsonluzfilho <gilsonfilho1994@yahoo.com.br>
2023-11-19 17:04:38 -0300

    validacao do email: obrigatorio e valido

--- scripts.js
+++ scripts.js
@@ -20,6 +20,14 @@ function checkInputs() {
     } else {
         setSuccessFor(username);
     };
+
+    if(emailValue === "") {
+        setErrorFor(email, 'O email é obrigatório.');
+    } else if(!checkEmail(emailValue)) {
+        setErrorFor(email, 'Por favor, insira um email válido.');
+    } else {
+        setSuccessFor(email);
+    };
 };
 
 function setErrorFor(input, message) {
@@ -38,4 +46,10 @@ function setSuccessFor(input) {
 
     // Adicionar a classe de sucesso
     formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
 }
